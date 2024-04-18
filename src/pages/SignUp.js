e87bf158699e09e10b5d295321fb62896c26c986
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-18 22:12:52 +0500

    all done | signup page img all done success

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,45 +1,46 @@
-import React, { useState } from 'react'
-import loginIcons from '../assest/signin.gif'
-import { FaEye } from "react-icons/fa";
-import { FaEyeSlash } from "react-icons/fa";
-import { Link, useNavigate } from 'react-router-dom';
-import SummaryApi from '../common';
-import { toast } from 'react-toastify';
+import React, { useState } from "react";
+import loginIcons from "../assest/signin.gif";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import SummaryApi from "../common";
+import { toast } from "react-toastify";
 
 const SignUp = () => {
-    const MAX_FILE_SIZE = 5 * 1024 * 1024;
-  const [showPassword,setShowPassword] = useState(false)
-  const [showConfirmPassword,setShowConfirmPassword] = useState(false)
-  const [data,setData] = useState({
-      email : "",
-      password : "",
-      name : "",
-      confirmPassword : "",
-      profilePic : "",
-  })
-  const navigate = useNavigate()
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+    name: "",
+    confirmPassword: "",
+    profilePic: "",
+  });
+  const [fileSizeError, setFileSizeError] = useState(false); // Define fileSizeError state
+  const navigate = useNavigate();
 
-  const handleOnChange = (e) =>{
-      const { name , value } = e.target
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
 
-      setData((preve)=>{
-          return{
-              ...preve,
-              [name] : value
-          }
-      })
-  }
+    setData((preve) => {
+      return {
+        ...preve,
+        [name]: value,
+      };
+    });
+  };
 
   const handleUploadPic = async (e) => {
     const file = e.target.files[0];
-  
+
     // Check if file size exceeds 2MB
     if (file.size > 2 * 1024 * 1024) {
-      console.error('File size exceeds 2MB limit.');
-      // Handle error or notify the user
+      setFileSizeError(true);
+      toast.error("Image size cannot exceed 2 MB");
       return;
     }
-  
+
+    setFileSizeError(false);
+
     try {
       const resizedImage = await new Promise((resolve) => {
         const reader = new FileReader();
@@ -48,12 +49,12 @@ const SignUp = () => {
           const img = new Image();
           img.src = event.target.result;
           img.onload = () => {
-            const canvas = document.createElement('canvas');
+            const canvas = document.createElement("canvas");
             const MAX_WIDTH = 300;
             const MAX_HEIGHT = 300;
             let width = img.width;
             let height = img.height;
-  
+
             if (width > height) {
               if (width > MAX_WIDTH) {
                 height *= MAX_WIDTH / width;
@@ -65,177 +66,175 @@ const SignUp = () => {
                 height = MAX_HEIGHT;
               }
             }
-  
+
             canvas.width = width;
             canvas.height = height;
-            const ctx = canvas.getContext('2d');
+            const ctx = canvas.getContext("2d");
             ctx.drawImage(img, 0, 0, width, height);
-            const resizedDataUrl = canvas.toDataURL('image/jpeg');
+            const resizedDataUrl = canvas.toDataURL("image/jpeg");
             resolve(resizedDataUrl);
           };
         };
       });
-  
+
       setData((prev) => ({
         ...prev,
         profilePic: resizedImage,
       }));
     } catch (error) {
-      console.error('Error resizing image:', error);
+      toast.error("Error resizing image:", error);
       // Handle error
     }
   };
-  
-  
-  
-  
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    if (data.password === data.confirmPassword) {
+      const dataResponse = await fetch(SummaryApi.signUP.url, {
+        method: SummaryApi.signUP.method,
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
 
-  const handleSubmit = async(e) =>{
-      e.preventDefault()
+      const dataApi = await dataResponse.json();
 
-      if(data.password === data.confirmPassword){
-
-        const dataResponse = await fetch(SummaryApi.signUP.url,{
-            method : SummaryApi.signUP.method,
-            headers : {
-                "content-type" : "application/json"
-            },
-            body : JSON.stringify(data)
-          })
-    
-          const dataApi = await dataResponse.json()
-
-          if(dataApi.success){
-            toast.success(dataApi.message)
-            navigate("/login")
-          }
-
-          if(dataApi.error){
-            toast.error(dataApi.message)
-          }
-    
-      }else{
-        toast.error("Please check password and confirm password")
+      if (dataApi.success) {
+        toast.success(dataApi.message);
+        navigate("/login");
       }
 
-  }
+      if (dataApi.error) {
+        toast.error(dataApi.message);
+      }
+    } else {
+      toast.error("Please check password and confirm password");
+    }
+  };
 
   return (
-    <section id='signup'>
-        <div className='mx-auto container p-4'>
+    <section id="signup">
+      <div className="mx-auto container p-4">
+        <div className="bg-white p-5 w-full max-w-sm mx-auto">
+          <div className="w-20 h-20 mx-auto relative overflow-hidden rounded-full">
+            <div>
+              <img src={data.profilePic || loginIcons} alt="login icons" />
+            </div>
+            <form>
+              <label>
+                <div className="text-xs bg-opacity-80 bg-slate-200 pb-4 pt-2 cursor-pointer text-center absolute bottom-0 w-full">
+                  Upload Photo
+                </div>
+                <input
+                className='hidden'
+                  type="file"
+                  accept="image/*"
+                  onChange={handleUploadPic}
+                />
+              </label>
+            </form>
+          </div>
 
-            <div className='bg-white p-5 w-full max-w-sm mx-auto'>
-
-                    <div className='w-20 h-20 mx-auto relative overflow-hidden rounded-full'>
-                        <div>
-                            <img src={data.profilePic || loginIcons} alt='login icons'/>
-                        </div>
-                        <form>
-                          <label>
-                            <div className='text-xs bg-opacity-80 bg-slate-200 pb-4 pt-2 cursor-pointer text-center absolute bottom-0 w-full'>
-                              Upload  Photo
-                            </div>
-                            <input type='file' accept='image/*' onChange={handleUploadPic} />
-                          </label>
-                        </form>
-                    </div>
-
-                    <form className='pt-6 flex flex-col gap-2' onSubmit={handleSubmit}>
-                      <div className='grid'>
-                              <label>Name : </label>
-                              <div className='bg-slate-100 p-2'>
-                                  <input 
-                                      type='text' 
-                                      placeholder='enter your name' 
-                                      name='name'
-                                      value={data.name}
-                                      onChange={handleOnChange}
-                                      required
-                                      className='w-full h-full outline-none bg-transparent'/>
-                              </div>
-                          </div>
-                        <div className='grid'>
-                            <label>Email : </label>
-                            <div className='bg-slate-100 p-2'>
-                                <input 
-                                    type='email' 
-                                    placeholder='enter email' 
-                                    name='email'
-                                    value={data.email}
-                                    onChange={handleOnChange}
-                                    required
-                                    className='w-full h-full outline-none bg-transparent'/>
-                            </div>
-                        </div>
-
-                        <div>
-                            <label>Password : </label>
-                            <div className='bg-slate-100 p-2 flex'>
-                                <input 
-                                    type={showPassword ? "text" : "password"} 
-                                    placeholder='enter password'
-                                    value={data.password}
-                                    name='password' 
-                                    onChange={handleOnChange}
-                                    required
-                                    className='w-full h-full outline-none bg-transparent'/>
-                                <div className='cursor-pointer text-xl' onClick={()=>setShowPassword((preve)=>!preve)}>
-                                    <span>
-                                        {
-                                            showPassword ? (
-                                                <FaEyeSlash/>
-                                            )
-                                            :
-                                            (
-                                                <FaEye/>
-                                            )
-                                        }
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div>
-                            <label>Confirm Password : </label>
-                            <div className='bg-slate-100 p-2 flex'>
-                                <input 
-                                    type={showConfirmPassword ? "text" : "password"} 
-                                    placeholder='enter confirm password'
-                                    value={data.confirmPassword}
-                                    name='confirmPassword' 
-                                    onChange={handleOnChange}
-                                    required
-                                    className='w-full h-full outline-none bg-transparent'/>
-
-                                <div className='cursor-pointer text-xl' onClick={()=>setShowConfirmPassword((preve)=>!preve)}>
-                                    <span>
-                                        {
-                                            showConfirmPassword ? (
-                                                <FaEyeSlash/>
-                                            )
-                                            :
-                                            (
-                                                <FaEye/>
-                                            )
-                                        }
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-
-                        <button className='bg-red-600 hover:bg-red-700 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6'>Sign Up</button>
-
-                    </form>
-
-                    <p className='my-5'>Already have account ? <Link to={"/login"} className=' text-red-600 hover:text-red-700 hover:underline'>Login</Link></p>
+          <form className="pt-6 flex flex-col gap-2" onSubmit={handleSubmit}>
+            <div className="grid">
+              <label>Name : </label>
+              <div className="bg-slate-100 p-2">
+                <input
+                  type="text"
+                  placeholder="enter your name"
+                  name="name"
+                  value={data.name}
+                  onChange={handleOnChange}
+                  required
+                  className="w-full h-full outline-none bg-transparent"
+                />
+              </div>
+            </div>
+            <div className="grid">
+              <label>Email : </label>
+              <div className="bg-slate-100 p-2">
+                <input
+                  type="email"
+                  placeholder="enter email"
+                  name="email"
+                  value={data.email}
+                  onChange={handleOnChange}
+                  required
+                  className="w-full h-full outline-none bg-transparent"
+                />
+              </div>
             </div>
 
+            <div>
+              <label>Password : </label>
+              <div className="bg-slate-100 p-2 flex">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="enter password"
+                  value={data.password}
+                  name="password"
+                  onChange={handleOnChange}
+                  required
+                  className="w-full h-full outline-none bg-transparent"
+                />
+                <div
+                  className="cursor-pointer text-xl"
+                  onClick={() => setShowPassword((preve) => !preve)}
+                >
+                  <span>{showPassword ? <FaEyeSlash /> : <FaEye />}</span>
+                </div>
+              </div>
+            </div>
 
+            <div>
+              <label>Confirm Password : </label>
+              <div className="bg-slate-100 p-2 flex">
+                <input
+                  type={showConfirmPassword ? "text" : "password"}
+                  placeholder="enter confirm password"
+                  value={data.confirmPassword}
+                  name="confirmPassword"
+                  onChange={handleOnChange}
+                  required
+                  className="w-full h-full outline-none bg-transparent"
+                />
+
+                <div
+                  className="cursor-pointer text-xl"
+                  onClick={() => setShowConfirmPassword((preve) => !preve)}
+                >
+                  <span>
+                    {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <button
+              disabled={fileSizeError}
+              className={`bg-red-600 hover:bg-red-700 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6 ${
+                fileSizeError ? "cursor-not-allowed opacity-50" : ""
+              }`}
+            >
+              Sign Up
+            </button>
+          </form>
+
+          <p className="my-5">
+            Already have account ?{" "}
+            <Link
+              to={"/login"}
+              className=" text-red-600 hover:text-red-700 hover:underline"
+            >
+              Login
+            </Link>
+          </p>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;

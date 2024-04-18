a26c03fb5328d5c3dfc018a0ca22f565ac79459d
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-18 21:08:07 +0500

    change footer latest

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='bg-slate-200'>
       <div className='container mx-auto p-4'>
-       <p className='text-center font-bold' title="Youtube Channel">Baraye Frokht</p>
+       <p className='text-center font-bold' title="Youtube Channel">&copy; 2024 Baraye Frokht. All rights reserved.</p>
       </div>
     </footer>
   )

ac073483487b60dd4d8b0eb0bc642f791801880f
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-18 09:42:56 +0500

    token all done success

--- src/components/AdminProductCard.js
+++ src/components/AdminProductCard.js
@@ -1,43 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 import { MdModeEditOutline } from "react-icons/md";
-import AdminEditProduct from './AdminEditProduct';
-import displayINRCurrency from '../helpers/displayCurrency';
+import AdminEditProduct from "./AdminEditProduct";
+import displayINRCurrency from "../helpers/displayCurrency";
 
-const AdminProductCard = ({ data, fetchdata, loggedInUserId }) => {
-    const [editProduct, setEditProduct] = useState(false);
+const AdminProductCard = ({ data, fetchDataCallback }) => {
+  const [editProduct, setEditProduct] = useState(false);
 
-    // Check if the product belongs to the logged-in user
-    const isUserProduct = data.userId === loggedInUserId;
 
-    // If the product does not belong to the logged-in user, don't render it
-    if (!isUserProduct) {
-        return null;
-    }
-
-    return (
-        <div className='bg-white p-4 rounded'>
-            <div className='w-40'>
-                <div className='w-32 h-32 flex justify-center items-center'>
-                    <img src={data?.productImage[0]} alt={data.productName} className='mx-auto object-fill h-full' />
-                </div>
-                <h1 className='text-ellipsis line-clamp-2'>{data.productName}</h1>
-
-                <div>
-                    <p className='font-semibold'>
-                        {displayINRCurrency(data.sellingPrice)}
-                    </p>
-
-                    <div className='w-fit ml-auto p-2 bg-green-100 hover:bg-green-600 rounded-full hover:text-white cursor-pointer' onClick={() => setEditProduct(true)}>
-                        <MdModeEditOutline />
-                    </div>
-                </div>
-            </div>
-
-            {editProduct && (
-                <AdminEditProduct productData={data} onClose={() => setEditProduct(false)} fetchdata={fetchdata} />
-            )}
+  return (
+    <div className="bg-white p-4 rounded">
+      <div className="w-40">
+        <div className="w-32 h-32 flex justify-center items-center">
+          <img
+            src={data?.productImage[0]}
+            alt={data.productName}
+            className="mx-auto object-fill h-full"
+          />
         </div>
-    );
+        <h1 className="text-ellipsis line-clamp-2">{data.productName}</h1>
+
+        <div>
+          <p className="font-semibold">
+            {displayINRCurrency(data.sellingPrice)}
+          </p>
+
+          <div
+            className="w-fit ml-auto p-2 bg-green-100 hover:bg-green-600 rounded-full hover:text-white cursor-pointer"
+            onClick={() => setEditProduct(true)}
+          >
+            <MdModeEditOutline />
+          </div>
+        </div>
+      </div>
+
+      {editProduct && (
+        <AdminEditProduct
+          productData={data}
+          onClose={() => setEditProduct(false)}
+          fetchData={fetchDataCallback}
+        />
+      )}
+    </div>
+  );
+};
+
+AdminProductCard.propTypes = {
+  data: PropTypes.object.isRequired,
+  fetchDataCallback: PropTypes.func.isRequired,
+  loggedInUserId: PropTypes.string.isRequired,
 };
 
 export default AdminProductCard;
--- src/pages/AllProducts.js
+++ src/pages/AllProducts.js
@@ -1,15 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import {jwtDecode} from 'jwt-decode';
-import UploadProduct from '../components/UploadProduct';
-import SummaryApi from '../common';
-import AdminProductCard from '../components/AdminProductCard';
+import React, { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
+import UploadProduct from "../components/UploadProduct";
+import SummaryApi from "../common";
+import AdminProductCard from "../components/AdminProductCard";
 
 const AllProducts = () => {
   const [openUploadProduct, setOpenUploadProduct] = useState(false);
   const [userProducts, setUserProducts] = useState([]);
   const [loggedInUserId, setLoggedInUserId] = useState(null);
 
-  // Function to fetch products
   const fetchAllProduct = async () => {
     try {
       const response = await fetch(SummaryApi.allProduct.url);
@@ -24,61 +23,61 @@ const AllProducts = () => {
   useEffect(() => {
     const fetchTokenAndUserId = async () => {
       try {
-        // Fetch the JWT token from local storage
-        const token = localStorage.getItem('token');
-        console.log('Token from local storage:', token); // Log the token value
+        const token = localStorage.getItem("token");
+        console.log("Token from local storage:", token); 
         if (token) {
-          // Decode the token to inspect its contents
           const decodedToken = jwtDecode(token);
-          console.log('Decoded token:', decodedToken); // Log the decoded token
-    
-          // Check if the decoded token contains the userId field
+          console.log("Decoded token:", decodedToken); 
+
           const userId = decodedToken?._id;
           if (userId) {
             setLoggedInUserId(userId);
-            console.log('Logged-in user ID:', userId); // Log the logged-in user ID
+            console.log("Logged-in user ID:", userId);
           } else {
-            console.log('User ID not found in token');
+            console.log("User ID not found in token");
           }
-        }        
+        }
       } catch (error) {
         console.error("Error fetching token:", error);
       }
     };
-    
-  
+
     fetchTokenAndUserId();
   }, []);
-  
 
   useEffect(() => {
     fetchAllProduct();
-  }, []); // This effect runs once on component mount to fetch products
+  }, []); 
 
   useEffect(() => {
     console.log("Initial user products:", userProducts);
   }, [userProducts]);
-  
 
   return (
     <div>
-      <div className='bg-white py-2 px-4 flex justify-between items-center'>
-        <h2 className='font-bold text-lg'>All Product</h2>
-        <button className='border-2 border-red-600 text-red-600 hover:bg-red-600 hover:text-white transition-all py-1 px-3 rounded-full ' onClick={() => setOpenUploadProduct(true)}>Upload Product</button>
+      <div className="bg-white py-2 px-4 flex justify-between items-center">
+        <h2 className="font-bold text-lg">All Product</h2>
+        <button
+          className="border-2 border-red-600 text-red-600 hover:bg-red-600 hover:text-white transition-all py-1 px-3 rounded-full "
+          onClick={() => setOpenUploadProduct(true)}
+        >
+          Upload Product
+        </button>
       </div>
 
-      {/**all product */}
-      <div className='flex items-center flex-wrap gap-5 py-4 h-[calc(100vh-190px)] overflow-y-scroll'>
+      <div className="flex items-center flex-wrap gap-5 py-4 h-[calc(100vh-190px)] overflow-y-scroll">
         {userProducts
-          .filter(product => product.userId === loggedInUserId)
+          .filter((product) => product.userId === loggedInUserId)
           .map((product, index) => (
             <AdminProductCard data={product} key={index + "allProduct"} />
           ))}
       </div>
 
-      {/**upload product component */}
       {openUploadProduct && (
-        <UploadProduct onClose={() => setOpenUploadProduct(false)} fetchData={fetchAllProduct} />
+        <UploadProduct
+          onClose={() => setOpenUploadProduct(false)}
+          fetchData={fetchAllProduct}
+        />
       )}
     </div>
   );

99baa6e06e8730c34327b0841f702aad64ab3dd5
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-18 11:58:46 +0500

    all done success

--- src/components/AdminEditProduct.js
+++ src/components/AdminEditProduct.js
@@ -7,13 +7,16 @@ import DisplayImage from './DisplayImage';
 import { MdDelete } from "react-icons/md";
 import SummaryApi from '../common';
 import {toast} from 'react-toastify'
+import { useNavigate } from 'react-router-dom';
+
 
 const AdminEditProduct = ({
     onClose,
     productData,
-    fetchdata
-  }) => {
+  }
+  ) => {
 
+  const navigate = useNavigate();
   const [data,setData] = useState({
     ...productData,
     productName : productData?.productName,
@@ -84,7 +87,10 @@ const AdminEditProduct = ({
 
     if(responseData.success){
         toast.success(responseData?.message)
-        onClose()
+        onClose();
+        navigate('/admin-panel/all-products');
+
+
     }
 
 

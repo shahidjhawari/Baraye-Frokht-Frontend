c081c6bc6584115049a5fc77f360ce5d0c5d2da7
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-23 17:51:38 +0500

    categ product display

--- src/pages/CategoryProduct.js
+++ src/pages/CategoryProduct.js
@@ -96,7 +96,7 @@ const CategoryProduct = () => {
     <div className='container mx-auto p-4'>
 
        {/***desktop version */}
-       <div className='hidden lg:grid grid-cols-[200px,1fr]'>
+       <div className=' lg:grid grid-cols-[200px,1fr]'>
            {/***left side */}
            <div className='bg-white p-2 min-h-[calc(100vh-120px)] overflow-y-scroll'>
                 {/**sort by */}

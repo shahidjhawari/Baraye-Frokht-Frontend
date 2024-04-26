3366d8d6fa60890aec6660011651b17a867cf268
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-26 10:00:40 +0500

    latest all done

--- src/helpers/productCategory.js
+++ src/helpers/productCategory.js
@@ -1,16 +1,14 @@
 const productCategory = [
-    { id : 1, label : "Airpodes", value : "mobiles"},
-    { id : 2, label : "Electronic", value : "electronic"},
-    { id : 3, label : "Earphones", value : "earphones"},
-    { id : 4, label : "Appliances", value : "appliances"},
-    { id : 5, label : "Bikes", value : "bikes"},
-    { id : 6, label : "Property", value : "property"},
-    { id : 7, label : "Services", value : "services"},
-    { id : 8, label : "Fashion", value : "fashion"},
-    { id : 9, label : "Books", value : "books"},
-    { id : 10, label : "Pets", value : "pets"},
-    { id : 11, label : "Other", value : "other"},
-]
+  { id: 1, label: "Mobiles", value: "mobiles" },
+  { id: 2, label: "Electronic", value: "electronic" },
+  { id: 3, label: "Appliances", value: "appliances" },
+  { id: 4, label: "Bikes", value: "bikes" },
+  { id: 5, label: "Property", value: "property" },
+  { id: 6, label: "Services", value: "services" },
+  { id: 7, label: "Fashion", value: "fashion" },
+  { id: 8, label: "Books", value: "books" },
+  { id: 9, label: "Pets", value: "pets" },
+  { id: 10, label: "Other", value: "other" },
+];
 
-
-export default productCategory
+export default productCategory;

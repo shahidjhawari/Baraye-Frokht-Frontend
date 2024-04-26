d57e51ec31f002a30dd338f81c8eb95c96a3d270
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-26 10:04:46 +0500

    cloudnry in link done

--- src/helpers/uploadImage.js
+++ src/helpers/uploadImage.js
@@ -1,4 +1,4 @@
-const url = `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME_CLOUDINARY}/image/upload`
+const url = `https://api.cloudinary.com/v1_1/dxampilpv/image/upload`
 
 const uploadImage  = async(image) => {
     const formData = new FormData()

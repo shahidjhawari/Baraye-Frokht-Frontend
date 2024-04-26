5a754cbc03077a4675cdac37f7a5a69c093d3020
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-26 08:10:36 +0500

    latest all done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer className='bg-slate-200'>
-      <div className='container mx-auto p-4'>
-       <p className='text-center font-bold' title="Youtube Channel">&copy; 2024 Baraye Frokht. All rights reserved.</p>
+    <footer className="bg-slate-200">
+      <div className="container mx-auto p-4">
+        <p className="text-center font-bold" title="Baraye Frokht">
+          &copy; 2024 Baraye Frokht. All rights reserved.
+        </p>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;

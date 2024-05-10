c76a19887bccaa3b042b962f9ec2341270e35d2c
Shahid Iqbal <si656581@gmail.com>
2024-05-10 19:00:13 +0500

    all done

--- src/components/BannerProduct.js
+++ src/components/BannerProduct.js
@@ -5,19 +5,18 @@ import image2 from '../assest/banner/img2.webp';
 import image3 from '../assest/banner/img3.jpg';
 import image4 from '../assest/banner/img4.jpg';
 import image5 from '../assest/banner/img5.webp';
-import image1Mobile from '../assest/banner/img1_mobile.jpg';
-import image2Mobile from '../assest/banner/img2_mobile.webp';
-import image3Mobile from '../assest/banner/img3_mobile.jpg';
-import image4Mobile from '../assest/banner/img4_mobile.jpg';
-import image5Mobile from '../assest/banner/img5_mobile.png';
-import banner1 from "../assest/banner1.png"
+import image1Mobile from '../assest/1.webp';
+import image2Mobile from '../assest/2.webp';
+import image3Mobile from '../assest/3.webp';
+import image4Mobile from '../assest/1.webp';
+import image5Mobile from '../assest/2.webp';
 
 const BannerProduct = () => {
     const [currentImage, setCurrentImage] = useState(0);
 
     const desktopImages = [image1, image2, image3, image4, image5];
 
-    const mobileImages = [banner1, image2Mobile, image3Mobile, image4Mobile, image5Mobile];
+    const mobileImages = [image1Mobile, image2Mobile, image3Mobile, image4Mobile, image5Mobile];
 
     const nextImage = () => {
         if (desktopImages.length - 1 > currentImage) {
